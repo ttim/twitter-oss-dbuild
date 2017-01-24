36bc59ed0a55c04778ea25e64d2e61adc03d19d2
Timur Abishev <tabishev@twitter.com>
2017-01-24 23:21:22 +0000

    fix different errors

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ set -e
 export JVM_OPTS="-XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -XX:ReservedCodeCacheSize=96m -XX:+TieredCompilation -XX:MaxPermSize=1024m -Xms512m -Xmx2048m -Xss2m"
 
 BASEDIR=$(dirname $(dirname $0))
-DBUILD_CONF="$BASEDIR/configuration/twitter-oss.dbuild"
+DBUILD_CONF="$BASEDIR/twitter-oss.dbuild"
 
 $BASEDIR/bin/dbuild $DBUILD_CONF 2>&1
